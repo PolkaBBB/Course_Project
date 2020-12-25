a6cfbb11523a0c461af57360e98f1ac24dc3206f
PolkaBBB <61475184+PolkaBBB@users.noreply.github.com>
2020-12-25 21:52:52 +0300

    Create Line.h

---
+++ header_files/Line.h
@@ -0,0 +1,26 @@
+#ifndef LINE_H
+#define LINE_H
+
+#include <QObject>
+#include <QGraphicsItemGroup>
+#include <QPen>
+#include <QBrush>
+
+class Line : public QGraphicsItemGroup, public QObject {
+public:
+    const int EllipseRadius = 3;
+    /*const*/ int LineWidth = 1;
+    QColor color_of_line;
+protected:
+    QPen m_pen;
+    QBrush m_brush;
+public:
+    Line(const QColor &color, QObject *parent = 0);
+    void removePoint();
+public slots:
+    void addPoint(QPointF point);
+protected:
+    QGraphicsEllipseItem *m_lastPoint;
+};
+
+#endif // LINE_H
